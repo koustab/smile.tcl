2b45efa5d7cee34d899823b8870bcc379cded8e8
koustab <koustab@gmail.com>
2021-12-02 11:04:00 +0000

    Create smile.tcl

---
+++ smile.tcl
@@ -0,0 +1,29 @@
+set rplytimer 3
+set chname  "#error"
+
+set autorply {
+"=;)"
+"@;)"
+"q;)"
+":-)"
+":-))"
+":-D"
+"&:-)"
+":-p"
+";-)"
+";-))"
+":-O"
+":-1"
+}
+
+bind pub - {:)} pub:smile
+bind pub - {=:)} pub:smile
+bind pub - {=;)} pub:smile
+bind pub - {=)} pub:smile
+bind pub - {:p} pub:smile
+
+proc pub:smile {nick host handle channel text} {
+ global autorply rplytimer chname
+ utimer $rplytimer  [list puthelp "PRIVMSG $chname :[lindex $autorply [rand [llength $autorply]]]"]
+}
+putlog "smile.tcl 0.1 by Error loaded"
